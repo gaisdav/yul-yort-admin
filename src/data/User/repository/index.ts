0444fe59b3732c82f74797cf19743b6a4aef3ac2
afdavletkulov <afdavletkulov@rttv.ru>
2022-07-07 12:15:27 +0500

    API login

--- src/data/User/repository/index.ts
+++ src/data/User/repository/index.ts
@@ -4,7 +4,11 @@ import { BaseRepository } from "src/data/BaseRepository";
 
 export class UserRepository extends BaseRepository implements IUserRepository {
   async login(): Promise<void> {
-    await this.api.post(EEndpoints.LOGIN);
+    const a = {
+      login: "test@mail.ru",
+      password: "123456",
+    };
+    await this.api.post(EEndpoints.LOGIN, a);
   }
   async logout(): Promise<void> {
     await this.api.post(EEndpoints.LOGOUT);
